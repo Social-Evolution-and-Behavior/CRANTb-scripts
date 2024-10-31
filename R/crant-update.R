793d3e4d6a09abfe253ae400c57301882a7a0ce0
alexanderbates <alexander.shakeel.bates@gmail.com>
2024-10-30 22:03:15 -0400

    Update crant-update.R

--- R/crant-update.R
+++ R/crant-update.R
@@ -4,15 +4,18 @@
 source("R/crant-startup.R")
 
 # save current version of the table
-ac <- cranttable_query()
+message("### crantb: saving seatable snapshot ###")
+ac <- crant_table_query()
 dir.create(file.path(crant.meta.save.path,"snapshots"), showWarnings = FALSE)
 current_datetime <- Sys.time()
 datetime_string <- format(current_datetime, "%Y-%m-%d")
 readr::write_csv(ac, file.path(file.path(crant.meta.save.path,"snapshots"),paste0(datetime_string,"_crantb_seatable.csv")))
 
 # run IDs update
+message("### crantb: updating root ids in crant table for crantb ###")
 crantr::crant_table_updateids()
 
 # run ngl link update for issue neurons
+message("### crantb: updating ngl links in crant table for crantb ###")
 crantr:::crant_table_update_tracing()
 
